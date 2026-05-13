a283901193bae70005eb5c3d8077c1c885e8d25d
Mizanur Rahman Sajid <sajid2016bsl@gmail.com>
2026-05-13 20:24:46 +0600

    Initial Changes

--- index.js
+++ index.js
@@ -3,12 +3,20 @@ import moment from 'moment';
 import simpleGit from 'simple-git';
 
 const path = './data.json';
-const date = moment().subtract(5, 'd').format();
+
+const markComit = (x, y) => {
+    const date = moment()
+    .subtract(1, "y")
+    .add(1, "d")
+    .add(x, "w")
+    .add(y, "d")
+    .format()
 
 const data = {
     date: date,
 };
 
-jsonfile.writeFile(path, data, ()=>{
-    simpleGit().add([path]).commit(date,{'--date': date}).push();
+jsonfile.writeFile(path, data, () => {
+    simpleGit().add([path]).commit(date, {"--date": date}).push();
 });
+};
